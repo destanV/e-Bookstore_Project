c25a6cb1bff0ee5bcfe1454afb9aa35d7f83895f
deniz <deniz.bakici@bilgiedu.net>
2024-12-29 17:16:30 +0300

    123 silindi.

--- shop.php
+++ shop.php
@@ -6,7 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>E-bookstore</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
@@ -14,6 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
         rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <style>
         .book-item {
             margin-bottom: 30px;
@@ -41,9 +41,7 @@
             margin-bottom: 10px;
         }
 
-        .container {
-            margin-top: 30px;
-        }
+
     </style>
 </head>
 <body>
@@ -53,7 +51,7 @@
     <main>
         <?php include 'reusables/searchbar copy.php'; ?>
 
-        <div class="container">
+        <div class="container" style="margin-top:3rem;">
             <h1 class="mb-4">Shop Page</h1>
             <div id="book-list"></div>
         </div>
