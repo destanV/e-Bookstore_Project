a4bc52a79d88135503c14793db0e7dcfe925e267
Pınar Sude Gürdamar <80318942+pinargurdamar@users.noreply.github.com>
2025-01-18 22:18:01 +0300

    Update searchbar.php

--- reusables/searchbar.php
+++ reusables/searchbar.php
@@ -1,3 +1,32 @@
+<?php
+// Database connection
+$db_host = 'localhost'; 
+$db_user = 'ewb_user';           
+$db_pass = 'abc1';
+$db_name = 'ewb';               
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch all languages for the dropdown
+$language_query = "SELECT language_id, language_name FROM language";
+$language_result = mysqli_query($conn, $language_query);
+$languages = [];
+
+if (!$language_result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+while ($row = mysqli_fetch_assoc($language_result)) {
+    $languages[] = $row;
+}
+
+$conn->close();
+?>
+
 <div class="container mt-5">
     <div class="row">
         <div class="col-12">
@@ -28,8 +57,11 @@
                             <div class="col-md-4">
                                 <select class="form-select" id="language-filter" name="language_filter">
                                     <option value="" selected>Choose Language</option>
-                                    <option value="4">Turkish</option>
-                                    <option value="5">English</option>
+                                    <?php foreach ($languages as $language): ?>
+                                        <option value="<?= $language['language_id'] ?>" <?= isset($_GET['language_filter']) && $_GET['language_filter'] == $language['language_id'] ? 'selected' : '' ?>>
+                                            <?= htmlspecialchars($language['language_name']) ?>
+                                        </option>
+                                    <?php endforeach; ?>
                                 </select>
                             </div>
 
